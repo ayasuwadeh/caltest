1632018a878eb52bfd1ccce036c97571b02c891b
Aya <Aya@192.168.1.15>
2020-02-27 01:09:43 +0200

    first test

---
+++ src/cal/Test.java
@@ -0,0 +1,11 @@
+package cal;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		Calculator temp=new Calculator();
+	int result=	temp.sum(2, 3);
+	System.out.print(result);
+	}
+}
